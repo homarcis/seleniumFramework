d70cf95139f4e4e43914ea89051283fcd828dd6c
homarcis <homarcis@hotmail.com>
2021-03-25 22:58:07 -0600

    nuevos cambios

---
+++ src/test/java/stepdefinitions/SignUpStepDefinition.java
@@ -0,0 +1,25 @@
+package stepdefinitions;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class SignUpStepDefinition {
+
+    @Given("^Client wants to have an account$")
+    public void client_wants_to_have_an_account() throws Throwable {
+
+    }
+
+    @When("^he send required information to get the account$")
+    public void he_send_required_information_to_get_the_account() throws Throwable {
+
+    }
+
+    @Then("^he should be told that the account was created$")
+    public void he_should_be_told_that_the_account_was_created() throws Throwable {
+
+    }
+
+
+}
