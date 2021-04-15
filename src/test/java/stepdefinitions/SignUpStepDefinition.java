7070c7bdddc0495b08e0e03c4c211aec53dbb569
homarcis <homarcis@hotmail.com>
2021-04-15 13:42:36 -0500

    Cambio menor

--- src/test/java/stepdefinitions/SignUpStepDefinition.java
+++ src/test/java/stepdefinitions/SignUpStepDefinition.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import pageobjects.SignUpServices;
 import pageobjects.SingUpPageObject;
+import pageobjects.SingUpPageObject;
 import util.HookDriver;
 import util.RandomNumberGenerator;
 
